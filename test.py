33ddb6157f345b1657208f058bc664188a810d6f
rogerlcs <rogerleo334@gmail.com>
2022-04-07 18:19:16 -0300

    Att

--- test.py
+++ test.py
@@ -1,36 +1,45 @@
-from requests import Request, Session
+import threading
+import requests
 import colorama
 from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
+colorama.init()
+
+
+
 
 def checkstatus(host, proxy):
+    result = open("result.txt","at")
     proxies = {"http" : "http://%s:80" % (proxy),
                "https" : "http://%s:80" % (proxy)}
     try:
-        s = Session()
-        r = Request('CONNECT',"http://"+host)
-        prepped = r.prepare()
-        resp = s.send(prepped, proxies = proxies, timeout = 3) 
-        return resp.status_code
-    except OSError:
-        return "400"
-    else:
-        return "erro"
+        r = requests.get("http://"+host, proxies = proxies, timeout = 3)
+        if r.status_code == 200:
+            print(Fore.GREEN + "%s,%s - %s\n" % (host,proxy,r.status_code))
+            result.write("%s,%s - 200OK\n" % (host,proxy))
+        else:
+            print(Fore.RED + "%s,%s - %s\n" % (host,proxy,r.status_code))
+        result.close()
+    except:
+        print(Fore.RED + "%s,%s - 400\n" % (host,proxy))
+        result.close()
     
 
 
 def main():
-    nomearq = str(input("Arquivo:"))
-    with open(nomearq,'rt') as file:
+    proxyarq = str(input("Arquivo proxy:"))
+    hostarq = str(input("Arquivo host:"))
+    with open(proxyarq,'rt') as file:
         line = file.readline()
         while line:
-            line = line.strip('\n')
-            host,proxy = line.split(',')
-            status = checkstatus(host, proxy)
-            if status == 200:
-                print(Fore.GREEN + "%s - %s" % (line,status))
-            else:
-                print(Fore.RED + "%s - %s" % (line,status))
+            proxy = line.strip('\n')
+            with open(hostarq,'rt') as file2:
+                line2 = file2.readline()
+                while line2:
+                    host = line2.strip('\n')
+                    if len(threading.enumerate()) < 10:
+                        t = threading.Thread(target = checkstatus, args = (host,proxy))
+                        t.start()
+                        line2 = file2.readline()
             line = file.readline()
 
 main() 
